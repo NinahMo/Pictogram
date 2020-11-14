016cc7b83fab15d3a1ae8659a73c73b033d9a829
Ashley Mosoti <ninahb746@gmail.com>
2020-11-15 01:17:51 +0300

    Add bootstrap for styling the app

--- mapicha/settings.py
+++ mapicha/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pics.apps.PicsConfig',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
