e56fc0b68fea8e84f124d03017b6c49716a4df88
Ashley Mosoti <ninahb746@gmail.com>
2020-11-15 00:47:24 +0300

    Create a template for the home page

--- pics/views.py
+++ pics/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to Pictogram')
+    return render(request, 'welcome.html')
