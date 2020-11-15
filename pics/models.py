c1c199a6ed71bcb9c1b065b30c88da8020c59b9a
Ashley Mosoti <ninahb746@gmail.com>
2020-11-15 19:06:39 +0300

    Create a pics class

--- pics/models.py
+++ pics/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Gallery(models.Model):
+    name = models.CharField(max_length=400, null=True, blank=True)
+    description = models.TextField(max_lemgth=400, null=True, blank=True)
+    location = models.ForeignKey('location',on_delete=models.CASCADE)
+    categories = models.ForeignKey('categories',on_delete=models.CASCADE)
+    
