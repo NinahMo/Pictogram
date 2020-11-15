d3986fafa1e20d2d3360601fe6891df0cae63322
Ashley Mosoti <ninahb746@gmail.com>
2020-11-15 19:21:48 +0300

    Make migrations to solve a bug

--- pics/models.py
+++ pics/models.py
@@ -4,11 +4,4 @@ from django.db import models
 class Pics(models.Model):
     name = models.CharField(max_length=400, null=True, blank=True)
     description = models.TextField(max_length=400, null=True, blank=True)
-    location = models.ForeignKey('location',on_delete=models.CASCADE)
-    categories = models.ForeignKey('categories',on_delete=models.CASCADE)
 
-class location(models.Model):
-    location = models.TextField(max_length=200)
-
-class categories(models.Model):
-    categories = models.TextField(max_length=200)
