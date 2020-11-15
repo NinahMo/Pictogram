1929dacd250b8dd2692cc75c29fccdf76c417db7
Ashley Mosoti <ninahb746@gmail.com>
2020-11-15 19:46:06 +0300

    Create a one to many relationship with Pics model

--- pics/models.py
+++ pics/models.py
@@ -4,4 +4,19 @@ from django.db import models
 class Pics(models.Model):
     name = models.CharField(max_length=400, null=True, blank=True)
     description = models.TextField(max_length=400, null=True, blank=True)
+    location = models.ForeignKey('location',on_delete=models.CASCADE)
+    category =models.ForeignKey('categories',on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+    class Meta:
+        ordering = ['name']
+
+class location(models.Model):
+    location = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.location
+    
+class categories(models.Model):
+    categories = models.TextField(max_length=200)
