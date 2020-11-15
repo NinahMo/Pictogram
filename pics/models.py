8cc39971ea02c21e6a048d0fd3ba48a55c9335f4
Ashley Mosoti <ninahb746@gmail.com>
2020-11-15 19:12:38 +0300

    Create a model classes for location and categories

--- pics/models.py
+++ pics/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class Gallery(models.Model):
+class Pics(models.Model):
     name = models.CharField(max_length=400, null=True, blank=True)
-    description = models.TextField(max_lemgth=400, null=True, blank=True)
+    description = models.TextField(max_length=400, null=True, blank=True)
     location = models.ForeignKey('location',on_delete=models.CASCADE)
     categories = models.ForeignKey('categories',on_delete=models.CASCADE)
-    
+
+class location(models.Model):
+    location = models.TextField(max_length=200)
+
+class categories(models.Model):
+    categories = models.TextField(max_length=200)
