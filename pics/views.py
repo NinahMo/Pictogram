973d83b951fa38620fb3cbe8bde030496b8536d6
Ashley Mosoti <ninahb746@gmail.com>
2020-11-17 14:12:31 +0300

    Rectify search term method

--- pics/views.py
+++ pics/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
-from .models import Pics
+from .models import Pics,categories
 
 # Create your views here.
 def welcome(request):
@@ -19,6 +19,10 @@ def search_results(request):
     if 'categories' in request.GET and request.GET["categories"]:
         search_term = request.GET.get("categories")
         searched_categories = categories.search_by_categories(search_term)
-        message = f"{search_term}}"
+        message = f"{search_term}"
 
         return render(request, 'search.html',{"message":message,"categories":searched_categories})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
